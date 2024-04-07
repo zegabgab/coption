b4fc46a83386e8b2edab3fe75b5687b5899ff4ab
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-04-08 00:02:36 +0200

    Completed the generic option. By the way, please do not actually use this

--- src/option.c
+++ src/option.c
@@ -8,10 +8,39 @@ Option *opt_empty() {
     return opt_with(NULL);
 }
 
-Option *opt_with(void *value) {
+Option *opt_with(
+        void *value) {
     Option *opt = malloc(sizeof(Option));
     if (opt != NULL) {
-        opt-> data = value;
+        opt->data = value;
     }
     return opt;
+}
+
+int opt_has(const Option *option) {
+    return option->data != NULL;
+}
+
+void *opt_unwrap(
+        const Option *option) {
+    return option->data;
+}
+
+void *opt_or(
+        const Option *option, void *default_value) {
+    return opt_has(option) ? option->data : default_value;
+}
+
+Option *opt_map(
+        const Option *option, void *(*function)(void*)) {
+    return opt_has(option) ?
+        opt_with(function(option->data)) :
+        opt_empty();
+}
+
+Option *opt_map_closure(
+        const Option *option, void *context, void *(*function)(void*, void*)) {
+    return opt_has(option) ?
+        opt_with(function(option->data, context)) :
+        opt_empty();
 }
--- src/option.h
+++ src/option.h
@@ -13,39 +13,27 @@ Option *opt_empty();
 
 // Basic constructor with data. Returns an empty if value is null
 Option *opt_with(
-        void *value
-        );
+        void *value);
 
 // Returns a boolean value that indicates whether the option has any data
-void *opt_has(
-        const Option option
-        );
+int opt_has(
+        const Option *option);
 
 // Returns the contained value if there is one. Calling this on a value whose
 // option_has returns 0 is undefined behaviour.
 void *opt_unwrap(
-        const Option option
-        );
+        const Option *option);
 
 // Returns the contained value if there is one, or default_value if there is none.
 void *opt_or(
-        const Option option, int default_value
-        );
+        const Option *option, void *default_value);
 
 // Maps a raw function pointer to option's data, if it exists
 Option *opt_map(
-        const Option option, int (*function)(int)
-        );
+        const Option *option, void *(*function)(void*));
 
 // Maps a function pointer with context to the option
 Option *opt_map_closure(
-        const Option *option, void *context, int (*function)(void*, void*)
-        );
-
-// Prints the Option to the given buffer, for up to n characters.
-// Returns the number of characters printed.
-int opt_snprint(
-        char *buffer, size_t buffer_size, const Option *option
-        );
+        const Option *option, void *context, void *(*function)(void*, void*));
 
 #endif
